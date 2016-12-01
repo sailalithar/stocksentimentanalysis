64261ea26a2273c653d31b29652bcffd3a902c3b
Nishit Prasad <nshtpd@gmail.com>
2016-11-30 22:35:40 -0500

    Update LR_V3.py

--- LR_V3.py
+++ LR_V3.py
@@ -11,6 +11,9 @@ from sklearn import metrics
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.cross_validation import cross_val_score
 import math
+import matplotlib.pyplot as plt
+#import matplotlib.pylab as plt
+import plotly.plotly as py
 
 def getX(rawData, rawDataSnP):
     
@@ -205,9 +208,8 @@ def evaluate(stocks, start, end):
     finalList = []
     finalListSnP = []
     for stock in stocks:
-
         rawData = web.DataReader([stock], 'yahoo',start, end)
-
+        #print("xx"+ str(stock))
         rawData = rawData.to_frame()
         
         #S&P
@@ -215,7 +217,7 @@ def evaluate(stocks, start, end):
     
         #get X matrix
         df = getX(rawData, rawDataSnP)
-        
+        #print("xxx"+ str(stock))
         #convert dataframe to training matrix
         X = pd.DataFrame.as_matrix(df)
     
@@ -262,13 +264,159 @@ def evaluate(stocks, start, end):
                             'test_accSnP': test_accSnP
                         }
                     )
-                    
     return finalList, finalListSnP        
     
 if __name__ == "__main__":
     
-    #Stock List
-    stocks = ['FB', 'AAPL', 'MSFT', 'IBM']
+    '''
+    Capital Goods Companies (Top 10 market capital) (3)
+    
+       Tesla Motors, Inc.
+       PACCAR Inc.
+       Illumina, Inc.    
+    '''
+    capGoodsStock = ['TSLA', 'PCAR', 'ILMN']
+    
+    '''
+    Consumer Non-Durables (3)
+        
+        The Kraft Heinz Company
+        Mondelez International, Inc.
+        Monster Beverage Corporation
+    
+    Consumer Services (22)
+    
+        Amazon.com, Inc.
+        Comcast Corporation
+        Starbucks Corporation
+        Charter Communications, Inc.
+        Costco Wholesale Corporation
+        Twenty-First Century Fox, Inc.
+        Netflix, Inc.
+        Twenty-First Century Fox, Inc.
+        Marriott International
+        Liberty Global plc
+        Ross Stores, Inc.
+        DISH Network Corporation
+        Reilly Automotive, Inc.
+        JD.com, Inc.
+        Equinix, Inc.
+        Sirius XM Holdings Inc.
+        Paychex, Inc.
+        Dollar Tree, Inc.
+        Expedia, Inc.
+        Viacom Inc.
+        Ulta Salon, Cosmetics & Fragrance, Inc.
+        Fastenal Company    
+    '''
+    consServNDStock = ['KHC', 'MDLZ', 'MNST', 'AMZN', 'CMCSA', 'SBUX', 'CHTR',
+                       'COST', 'FOX', 'NFLX', 'FOXA', 'MAR', 'LBTYA', 'ROST',
+                       'DISH', 'ORLY', 'JD', 'EQIX', 'SIRI', 'PAYX', 'DLTR',
+                       'EXPE', 'VIA', 'ULTA', 'FAST']
+
+    '''
+    Finance (7)
+                 
+        CME Group Inc.
+        TD Ameritrade Holding Corporation
+        Fifth Third Bancorp
+        Northern Trust Corporation
+        T. Rowe Price Group, Inc.
+        Interactive Brokers Group, Inc.
+        Huntington Bancshares Incorporated
+    '''
+    finStock = ['CME', 'AMTD', 'FITB', 'NTRS', 'TROW', 'IBKR', 'HBAN']
+    
+    '''
+    Health Care (15)
+    
+        Amgen Inc.
+        Gilead Sciences, Inc.
+        Celgene Corporation
+        Walgreens Boots Alliance, Inc.
+        Biogen Inc.
+        Express Scripts Holding Company
+        Regeneron Pharmaceuticals, Inc.
+        Alexion Pharmaceuticals, Inc.
+        Intuitive Surgical, Inc.
+        Vertex Pharmaceuticals Incorporated
+        Incyte Corporation
+        Mylan N.V.
+        Shire plc
+        BioMarin Pharmaceutical Inc.
+        DENTSPLY SIRONA Inc.
+    '''
+    hCareStock = ['AMGN', 'GILD', 'CELG', 'WBA', 'BIIB', 'ESRX', 'REGN', 'ALXN',
+                  'ISRG', 'VRTX', 'INCY', 'MYL', 'SHPG', 'BMRN', 'XRAY' ]
+             
+    '''
+    Miscellaneous (5)
+     
+        The Priceline Group Inc. 
+        PayPal Holdings, Inc.
+        eBay Inc.
+        Ctrip.com International, Ltd.
+        NetEase, Inc.
+    '''
+    miscStock = ['PCLN', 'PYPL', 'EBAY', 'CTRP', 'NTES']
+
+    '''
+    Public Utilities (1)
+    
+        T-Mobile US, Inc.
+    '''
+    pubUtilStock = ['TMUS']
+    
+    '''
+    Technology (30)
+    
+        Apple Inc.
+        Google
+        Microsoft Corporation
+        Facebook, Inc.
+        Intel Corporation
+        Cisco Systems, Inc.
+        QUALCOMM Incorporated
+        Texas Instruments Incorporated
+        Broadcom Limited
+        Adobe Systems Incorporated
+        NVIDIA Corporation
+        Baidu, Inc.
+        Automatic Data Processing, Inc.
+        Yahoo! Inc.
+        Applied Materials, Inc.
+        Cognizant Technology Solutions Corporation
+        NXP Semiconductors N.V.
+        Intuit Inc.
+        Activision Blizzard, Inc
+        Electronic Arts Inc.
+        Fiserv, Inc.
+        Analog Devices, Inc.
+        Micron Technology, Inc.
+        Western Digital Corporation
+        Lam Research Corporation
+        Cerner Corporation
+        Autodesk, Inc.
+        Symantec Corporation
+        Linear Technology Corporation
+        IHS Markit Ltd.
+    '''
+    techStock = ['AAPL', 'GOOGL', 'MSFT', 'FB', 'INTC', 'CSCO', 'QCOM', 'TXN',
+                 'AVGO', 'ADBE', 'NVDA', 'BIDU', 'ADP', 'YHOO', 'AMAT', 'CTSH',
+                 'NXPI', 'INTU', 'ATVI', 'EA', 'FISV', 'ADI', 'MU', 'WDC', 'LRCX',
+                 'CERN', 'ADSK', 'SYMC', 'LLTC', 'INFO']
+
+    '''
+    Transportation (2)
+    
+        CSX Corporation
+        American Airlines Group, Inc.
+    '''
+    transStock = ['CSX','AAL']
+
+    #Total Stock List
+    stocks = capGoodsStock + consServNDStock + finStock + hCareStock + miscStock + pubUtilStock + techStock + transStock
+    
     
     #Start and end Time
     start = datetime.date(2015,11,1)
@@ -278,6 +426,20 @@ if __name__ == "__main__":
 
     finalList, finalListSnP = evaluate(stocks, start, end)
     
-    print finalList
-    print "\n"
-    print finalListSnP
+    with open('final.txt', 'w') as fp:
+        for item in finalList:
+            fp.write("%s\n" % item)
+    
+    my_xticks = [item['stock'] for item in finalList]
+    y = [item['test_acc'] for item in finalList]
+    x = [i+1 for i in range(len(my_xticks))]
+    x = np.array(x)
+    y = np.array(y)
+    
+
+    plt.bar(x, y, align='center')
+    plt.xticks(x, my_xticks, size='small')    
+    plt.show()
+    
+    #fig = plt.gcf()
+    #plot_url = py.plot_mpl(fig, filename='mpl-basic-bar')
